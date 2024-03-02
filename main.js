72c85fe65424fcca1d02f10a37c57f5c4ebe0924
Nick Coats <coats.18@gmail.com>
2024-03-02 13:22:38 -0500

    got basic space game working again

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 import './style.css';
-// import { Scene } from './game/Scene';
-// import * as PIXI from 'pixi.js'
+import { Scene } from './game/Scene';
+import * as PIXI from 'pixi.js'
+
+let scene = new Scene('scene', 800, 600)
+scene.tick();
+
 // import * as Body from './matter-example/Body'
 
-// WORKING GAME
-import { Game } from './matter-example/Game';
-let game = new Game();
-// END WORKING GAME
+// EXAMPLE MATTER GAME
+// import { Game } from './matter-example/Game';
+// let game = new Game();
+// END EXAMPLE MATTER GAME
 
